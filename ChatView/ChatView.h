ba1943e95a1ff091a9ead14131c77c37690378b8
GeorgeKyrylenko <dinozawrua@gmail.com>
2018-07-26 10:25:43 +0300

    first commit

---
+++ ChatView/ChatView.h
@@ -0,0 +1,19 @@
+//
+//  ChatView.h
+//  ChatView
+//
+//  Created by George Kyrylenko on 7/19/18.
+//  Copyright © 2018 George Kyrylenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChatView.
+FOUNDATION_EXPORT double ChatViewVersionNumber;
+
+//! Project version string for ChatView.
+FOUNDATION_EXPORT const unsigned char ChatViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatView/PublicHeader.h>
+
+
